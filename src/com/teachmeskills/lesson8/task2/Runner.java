aef545f6ba110ee7b8c140d94ff78b9ead50053d
Grouzdev Alexander <71078615+bugivugiman@users.noreply.github.com>
2023-08-28 12:26:29 +0300

    Update Runner.java
    cosmetics changes

--- src/com/teachmeskills/lesson8/task2/Runner.java
+++ src/com/teachmeskills/lesson8/task2/Runner.java
@@ -7,9 +7,9 @@ import com.teachmeskills.lesson8.task2.model.roleProvider;
 
 public class Runner {
     public static void main(String[] args) {
-        roleProvider human1 = new director();
-        roleProvider human2 = new accountant();
-        roleProvider human3 = new worker();
+        roleProvider human1 = new Director();
+        roleProvider human2 = new Accountant();
+        roleProvider human3 = new Worker();
 
         human1.getProfession();
         human2.getProfession();
