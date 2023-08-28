ab24ba6b45b2f660e5b06cbb2641ac012ca1fd00
Grouzdev Alexander <71078615+bugivugiman@users.noreply.github.com>
2023-08-28 12:25:15 +0300

    Update worker.java
    name method

--- src/com/teachmeskills/lesson8/task2/model/worker.java
+++ src/com/teachmeskills/lesson8/task2/model/worker.java
@@ -1,6 +1,6 @@
 package com.teachmeskills.lesson8.task2.model;
 
-public class worker implements roleProvider{
+public class Worker implements roleProvider{
     public void getProfession(){
         System.out.println("Worker");
     }
